7b225183ca168487495ec9382ad083cb1546625b
Quarkk77 <ianschiltz232@gmail.com>
2025-09-22 01:34:02 -0400

    debug info

--- src/neej/Cpu.java
+++ src/neej/Cpu.java
@@ -6,6 +6,9 @@ public class Cpu
 	private int accumulator = 0x0000;
 
 	public String toString() {
-		return "Program Counter: " + programCounter + " Accumulator: " + accumulator;
+		String pCounterFormatted = Integer.toHexString(programCounter).toUpperCase();
+		String accumulatorFormatted = Integer.toHexString(accumulator).toUpperCase();
+
+		return "Program Counter: $" + pCounterFormatted + " Accumulator: $" + accumulatorFormatted;
 	}
 }

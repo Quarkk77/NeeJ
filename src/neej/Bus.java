10cab42dd09cc30a3c208b65c962c8f83a64c014
Quarkk77 <ianschiltz232@gmail.com>
2025-09-22 00:32:15 -0400

    bleh

---
+++ src/neej/Bus.java
@@ -0,0 +1,9 @@
+package neej;
+
+public class Bus
+{
+	public static void helloWorld()
+	{
+		System.out.println("Hello from NES bus!");
+	}
+}
--- src/neej/Main.java
+++ src/neej/Main.java
@@ -6,5 +6,6 @@ public class Main
 	{
 		System.out.println("Hello from main!");
 		Cpu.helloWorld();
+		Bus.helloWorld();
 	}
 }

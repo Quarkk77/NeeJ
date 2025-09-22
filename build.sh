3e56f8583b458148a00202a6d4469740f3f78e5d
Quarkk77 <ianschiltz232@gmail.com>
2025-09-22 00:09:35 -0400

    fixing build environment

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-set -e
 
-# compile into ./out
-mkdir -p out
-javac -d out $(find src -name "*.java")
+# Clean previous build
+rm -rf out/*
 
-# run main
-java -cp out neeJ.App
+# Compile Java files
+javac -d out src/neej/*.java
 
+# Check if compilation succeeded
+if [ $? -eq 0 ]; then
+    echo "Build successful. Running program..."
+    java -cp out neej.Main
+else
+    echo "Build failed."
+    exit 1
+fi

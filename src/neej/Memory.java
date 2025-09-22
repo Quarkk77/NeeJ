0439c64b42d6e710a8725b7797fdced12accb163
Quarkk77 <ianschiltz232@gmail.com>
2025-09-22 13:19:51 -0400

    guh

--- src/neej/Memory.java
+++ src/neej/Memory.java
@@ -3,6 +3,10 @@ package neej;
 public class Memory
 {
 	private int[] ram = new int[0x0800];
+	
+	// Create registers
+	private int[] ppu = new int[0x0008];
+	private int[] apu = new int[0x0020];
 
 	public int read(int index, int[] ram)
 	{
